1f641d0acb9efc842ca31613b4a7d7d084b88b1f
Victor Hearn-Yeates <victor.hearn-yeates@rpharms.com>
2020-01-09 16:27:23 +0000

    Added 'git add' to gira

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const postComment = require('./jira-data');
 const args = process.argv.slice(2);
 const [jiraNo, comment] = args;
 
+if (args[0] == '-a') {
+  console.log('Adding to git');
+  shell.exec(`git add ${args.slice(1).join(' ')}`);
+  process.exit(0);
+}
+
 if (jiraNo === undefined || comment == undefined) {
     console.log("Please provide two arguments: the jira no, and a comment");
     process.exit(0);
-  }
+} else {
+  console.log('Committing to git');
+  shell.exec(`git commit -m "${jiraNo}: ${comment}"`);
 
-console.log('Committing to git');
-shell.exec(`git commit -m "${jiraNo}: ${comment}"`);
-
-console.log('Adding comment to jira');
-postComment(jiraNo, comment);
+  console.log('Adding comment to jira');
+  postComment(jiraNo, comment);
+}
